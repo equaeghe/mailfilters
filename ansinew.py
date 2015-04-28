fcf4c05783d8e32577c43488840370a200aefaad
Erik Quaeghebeur <mailfilters@equaeghe.nospammail.net>
2015-04-28 16:28:33 +0200

    add ansinew.py script

---
+++ ansinew.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3.4
+
+"""
+  ansinew.py: A script that takes as stdin-input an rfc822 compliant message
+  with parts that are encoded in a 'utf-8' charset and gives as stdout-output
+  the same message, but with the charset replaced by 'windows-1252'.
+
+  Copyright (C) 2015 Erik Quaeghebeur
+
+  This program is free software: you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free Software
+  Foundation, either version 3 of the License, or (at your option) any later
+  version. This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+  details. You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import sys
+import email
+
+# Check whether no arguments have been given to the script (it takes none)
+nargs = len(sys.argv)
+if len(sys.argv) is not 1:
+  raise SyntaxError("This script takes no arguments, you gave " + nargs - 1 + ".")
+
+# Read and parse the message from stdin
+msg = email.message_from_bytes(sys.stdin.buffer.read())
+
+# Transform 'utf-8' to 'windows-1252'
+msg.set_charset('windows-1252')
+
+# Check whether no errors were found in the message (parts)
+if len(msg.defects) > 0:
+  raise Exception("An error occurred.")
+
+# Send the modified message to stdout
+print(msg.as_bytes().decode(encoding='windows-1252'))
