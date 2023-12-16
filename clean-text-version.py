d9eca70a2ba7cd6b17df8e1fd2d406745caa2d9a
Erik Quaeghebeur <equaeghe@users.noreply.github.com>
2023-12-16 10:10:04 +0100

    update exchange warning in text mail cleaner

--- clean-text-version.py
+++ clean-text-version.py
@@ -65,10 +65,10 @@ tuewarning = re.compile(
     r"Thanks in advance for your cooperation\. "
     r"TU/e LIS Services\.\n(\n   \[\d\]: mailto:abuse@tue\.nl\n)?")
 exchangewarning_text = re.compile(
-    r"\[?[A-Z][^\n]+ .*@.*\. "
+    r"\[?[A-Z]{0,1}[^\n]*.*@.*\. "
     r"[A-Z][^\n]+<?https://aka\.ms/LearnAboutSenderIdentification>?\]?\n*")
 exchangewarning_html = re.compile(
-    r"[A-Z][^\n]+ .*@.*\. "
+    r"[A-Z]{0,1}[^\n]*.*@.*\. "
     r"\[ [A-Z][^\n]+\]\[1\](?:\s*\n)+"
     r"\s*\[1\]: https://aka.ms/LearnAboutSenderIdentification\n*")
 # random stuff
