151139883136dd30cbeca2c4be210841098f403c
Erik Quaeghebeur <equaeghe@users.noreply.github.com>
2026-09-10 22:33:26 +0200

    Move charset2.py to argument-based selection

--- charset2.py
+++ charset2.py
@@ -1,56 +1,53 @@
 #!/usr/bin/env python3
 
 """
-  charset2.py: A script that takes as stdin-input an rfc822 compliant message
-  that gives as stdout-output the same message, but with the charset replaced
-  by the target charset. Which charset is used depends on the ending of the
-  (symlink) name with which this script is called: 'ansinew', 'utf8', '…'.
+charset2.py: A script that takes as stdin-input an rfc822 compliant message
+that gives as stdout-output the same message, but with the charset replaced
+by the target charset. Which charset is used is given by the script's
+single argument: 'ansinew', 'utf8', '…'.
 
-  Copyright (C) 2020 Erik Quaeghebeur
+Copyright (C) 2026 Erik Quaeghebeur
 
-  This program is free software: you can redistribute it and/or modify it under
-  the terms of the GNU General Public License as published by the Free Software
-  Foundation, either version 3 of the License, or (at your option) any later
-  version. This program is distributed in the hope that it will be useful, but
-  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-  details. You should have received a copy of the GNU General Public License
-  along with this program. If not, see <http://www.gnu.org/licenses/>.
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version. This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+details. You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
-import sys
+import argparse
 import email
 import email.policy
-
+import sys
 
 CHARSETS = {
     "utf8": "utf-8",
     "ansinew": "windows-1252",
 }
 
-# Check whether no arguments have been given to the script (it takes none)
-nargs = len(sys.argv)
-if len(sys.argv) != 1:
-    raise SyntaxError(f"This script takes no arguments, you gave {nargs - 1}.")
-
-# Determine the charset
-scriptname = sys.argv[0]
-charset = None
-for ending in CHARSETS.keys():
-    if scriptname.endswith(ending):
-        charset = CHARSETS["ending"]
-        break
+parser = argparse.ArgumentParser(
+    description="Replace the charset used in a message's text parts."
+)
+parser.add_argument(
+    "charset", choices=CHARSETS.keys(), help="which charset to switch to"
+)
+args = parser.parse_args()
+charset = CHARSETS[args.charset]
 
 # define email policy
 email_policy = email.policy.EmailPolicy(
-  max_line_length=None, linesep="\r\n", refold_source='none')
+    max_line_length=None, linesep="\r\n", refold_source="none"
+)
 
 # Read and parse the message from stdin
 msg = email.message_from_bytes(sys.stdin.buffer.read(), policy=email_policy)
 
 # Replace charset string in Content-Type header
 for part in msg.walk():
-    if part.get_content_type() in {'text/plain', 'text/html'}:
+    if part.get_content_type() in {"text/plain", "text/html"}:
         part.set_charset(charset)
 
 # Check whether no errors were found in the message (parts)
