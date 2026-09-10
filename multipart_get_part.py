ebbb831f6b2523d48d03e19be18e4ebf17d9eb70
Erik Quaeghebeur <equaeghe@users.noreply.github.com>
2026-09-10 22:39:09 +0200

    Move multipart_get_part.py to argument-based selection

--- multipart_get_part.py
+++ multipart_get_part.py
@@ -1,42 +1,39 @@
 #!/usr/bin/env python3
 
 """
-  multipart_get_part.py: A script that takes as stdin-input an rfc822 compliant
-  multipart message and gives as stdout-output the same message, but with the
-  multipart replaced by the selected child subpart. Which subpart is selected
-  depends on the (symlink) name with which this script is called: its last
-  character must be a (zero-indexed) number in 0-9. The corresponding child
-  subpart is selected.
+multipart_get_part.py: A script that takes as stdin-input an rfc822 compliant
+multipart message and gives as stdout-output the same message, but with the
+multipart replaced by the selected child subpart. Which subpart is selected
+is given by the script's single (zero-indexed) integer argument.
 
-  Copyright (C) 2020 Erik Quaeghebeur
+Copyright (C) 2026 Erik Quaeghebeur
 
-  This program is free software: you can redistribute it and/or modify it under
-  the terms of the GNU General Public License as published by the Free Software
-  Foundation, either version 3 of the License, or (at your option) any later
-  version. This program is distributed in the hope that it will be useful, but
-  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-  details. You should have received a copy of the GNU General Public License
-  along with this program. If not, see <http://www.gnu.org/licenses/>.
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version. This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+details. You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
-import sys
+import argparse
 import email
 import email.policy
+import sys
 
-
-# Check whether no arguments have been given to the script (it takes none)
-nargs = len(sys.argv)
-if len(sys.argv) != 1:
-    raise SyntaxError(f"This script takes no arguments, you gave {nargs - 1}.")
-
-# Determine which part is selected
-scriptname = sys.argv[0]
-selected = int(scriptname[-1])
+parser = argparse.ArgumentParser(
+    description="Replace a multipart message by one of its child subparts."
+)
+parser.add_argument("part", type=int, help="zero-indexed subpart number to select")
+args = parser.parse_args()
+selected = args.part
 
 # define email policy
 email_policy = email.policy.EmailPolicy(
-  max_line_length=None, linesep="\r\n", refold_source='none')
+    max_line_length=None, linesep="\r\n", refold_source="none"
+)
 
 # Read and parse the message from stdin
 msg = email.message_from_bytes(sys.stdin.buffer.read(), policy=email_policy)
@@ -46,9 +43,13 @@ if not msg.is_multipart():
     raise ValueError("Message is not multipart.")
 
 # extract the selected subpart
-for k, subpart in enumerate(msg.iter_parts()):
+subpart = None
+for k, part in enumerate(msg.iter_parts()):
     if k == selected:
+        subpart = part
         break
+if subpart is None:
+    raise ValueError(f"Message does not have a subpart {selected}.")
 
 # replace the multipart by the selected subpart
 msg.clear_content()
