d46991fcbfd27ae37fee26abb738614d1b115967
Erik Quaeghebeur <equaeghe@users.noreply.github.com>
2026-09-10 22:22:22 +0200

    Move alternative2.py to argument-based selection

--- alternative2.py
+++ alternative2.py
@@ -1,54 +1,54 @@
 #!/usr/bin/env python3
 
 """
-  alternative2.py: A script that takes as stdin-input an rfc822 compliant
-  message with a 'multipart/alternative' part with 'text/<target>' or
-  'multipart/<target>' and/or other parts and gives as stdout-output the same
-  message, but with the first 'multipart/alternative' part replaced by the
-  first such target part. Which part will be output depends on the ending of
-  the (symlink) name with which this script is called: 'plain', 'html',
-  'calendar', 'related', or 'mixed'.
+alternative2.py: A script that takes as stdin-input an rfc822 compliant
+message with a 'multipart/alternative' part with 'text/<target>' or
+'multipart/<target>' and/or other parts and gives as stdout-output the same
+message, but with the first 'multipart/alternative' part replaced by the
+first such target part. Which part is used is given by the script's single
+argument: 'plain', 'html', 'calendar', 'related', or 'mixed'.
 
-  Copyright (C) 2020 Erik Quaeghebeur
+Copyright (C) 2026 Erik Quaeghebeur
 
-  This program is free software: you can redistribute it and/or modify it under
-  the terms of the GNU General Public License as published by the Free Software
-  Foundation, either version 3 of the License, or (at your option) any later
-  version. This program is distributed in the hope that it will be useful, but
-  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-  details. You should have received a copy of the GNU General Public License
-  along with this program. If not, see <http://www.gnu.org/licenses/>.
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version. This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+details. You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
-import sys
+import argparse
 import email
 import email.policy
+import sys
 
+ALT = "multipart/alternative"
+REL = "multipart/related"
 
-ALT = 'multipart/alternative'
-REL = 'multipart/related'
+TARGETS = {
+    "plain": "text/plain",
+    "html": "text/html",
+    "calendar": "text/calendar",
+    "related": "multipart/related",
+    "mixed": "multipart/mixed",
+}
 
-# Check whether no arguments have been given to the script (it takes none)
-nargs = len(sys.argv)
-if len(sys.argv) != 1:
-    raise SyntaxError(f"This script takes no arguments, you gave {nargs - 1}.")
-
-# Determine which text part should be used
-scriptname = sys.argv[0]
-target = None
-for ending in {'plain', 'html', 'calendar'}:
-    if scriptname.endswith(ending):
-        target = "text/" + ending
-for ending in {'related', 'mixed'}:
-    if scriptname.endswith(ending):
-        target = "multipart/" + ending
-if not target:
-    raise ValueError(f"Unknown scriptname '{scriptname}' requested.")
+parser = argparse.ArgumentParser(
+    description="Replace a 'multipart/alternative' part by one of its target parts."
+)
+parser.add_argument(
+    "target", choices=TARGETS.keys(), help="which alternative part to keep"
+)
+args = parser.parse_args()
+target = TARGETS[args.target]
 
 # define email policy
 email_policy = email.policy.EmailPolicy(
-  max_line_length=None, linesep="\r\n", refold_source='none')
+    max_line_length=None, linesep="\r\n", refold_source="none"
+)
 
 # Read and parse the message from stdin
 msg = email.message_from_bytes(sys.stdin.buffer.read(), policy=email_policy)
@@ -63,12 +63,12 @@ alt = None
 container = msg
 fix_main_content_type = False
 for part in msg.walk():
-    if (part.get_content_type() == REL) and (part.get_param('type') == ALT):
+    if (part.get_content_type() == REL) and (part.get_param("type") == ALT):
         container = part
         fix_main_content_type = True
         break
 for part in container.walk():
-    if (part.get_content_type() == ALT):
+    if part.get_content_type() == ALT:
         alt = part
         break
 
@@ -96,7 +96,7 @@ if target_missing:
 
 # As necessary, fix the 'multipart/related' part
 if fix_main_content_type:
-    container.set_param('type', target)
+    container.set_param("type", target)
 
 # Check whether no errors were found in the message (parts)
 if len(msg.defects) + len(container.defects) + len(alt.defects) > 0:
